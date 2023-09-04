d10a41effadff796b43e869aeef4db9e83cb3752
enzo <142436206+EnzoEduard@users.noreply.github.com>
2023-09-04 17:24:46 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+/*
+const filme = {
+ nome: "Five Nights at Freddy's - O Pesadelo Sem Fim",
+lançamento: 2023,
+direçao: ['Matthew Lillard'],
+elenco: ['Josh','Matthew','Elizabeth'],
+assistido: false
+}
+console.log(filme.nome);
+console.log(filme.lançamento);
+console.log(filme.direçao);
+console.log(filme.elenco);
+console.log(filme.assistido);
+*/
+/*
+const dados = {
+nome: "Enzo Eduardo dos Santos Costa",
+idade: 16,
+generoMusical: "hiphop",
+}
+console.log("o nome da pessoa é " , dados.nome + " ela tem" , dados.idade + " anos e gosta muito de " , dados.generoMusical);
+*/
+/*
+const filme = {
+    nome: "Five Nights at Freddy's - O Pesadelo Sem Fim",
+   lançamento: 2023,
+   direçao: ['Matthew Lillard'],
+   elenco: ['Josh','Matthew','Elizabeth'],
+   assistido: false
+   }
+   filme.personagens = ['william afton','mike schmidt','bonie']
+   
+   console.log(filme.elenco[0] + " fez " + filme.personagens[0]);
+   console.log(filme.elenco[1] + " fez " + filme.personagens[1]);
+   console.log(filme.elenco[2] + " fez " + filme.personagens[2]);
+   filme.elenco[0] = "xuxa";
+   console.log(filme.elenco[0]);
+   console.log(filme);
+   */
+  /*
+  const usuario = {
+    nome: 'prof',
+    idade: 25,
+    email: 'prof@senacrs.com.br',
+    cidade: 'sao paulo'
+  }
+  const novoUsuario = {
+    ...usuario,
+    nome: 'joao',
+    idade: 28
+  }
+  console.log(usuario);
+  console.log(novoUsuario);
+  */
+ /*
+   const listaDeNomes = ["mika", "paula", "vitor"]
+const copiaListaDeNomes = [...listaDeNomes]
+console.log(copiaListaDeNomes) //["mika", "paula", "vitor"]
+*/
+/*
+    function receber(dados){
+    const dadinhos = {
+        ...dados,
+nome: "Enzo Eduardo dos Santos Costa",
+ idade: 16, 
+  ...dados,
+ comidas: ['chocolate','maça','banana'],
+ amigo: {
+    bff: livia,
+    idadef: 17
+ }
+    }
+    console.log('o nome da pessoa é', (dadinhos.nome), 'e suas comidas favoritas sao', (dadinhos.comidas), 'seu melhor amigo se chama', (dadinhos.amigo.bff), 'e tem'), (dadinhos.amigo.idade)
+}
+
+console.log(receber);
+*/
+    
